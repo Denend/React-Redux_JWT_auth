4fcb6f7be4a01590ac539bceadaabc00f6a0baf4
Danylo Soloviov <dsoloviov@zindulka.com>
2019-09-05 19:21:48 +0200

    notifications added completely

--- src/redux/actions/getAllNotes.js
+++ src/redux/actions/getAllNotes.js
@@ -1,32 +1,33 @@
+import { notySuccess, notyError } from "../../lib/noty";
 const fetchAllNotes = () => {
-	return dispatch => {
-		const token = localStorage.getItem("my-jwt");
-		console.log(token);
-		if (token) {
-			return fetch(
-				"http://frontend-recruitment.one2tribe.pl:8080/api/v1/item",
-				{
-					method: "GET",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${token}`
-					}
-				}
-			)
-				.then(resp => resp.json())
-				.then(data => {
-					console.log(data);
-					dispatch({
-						type: "GET_ALL_NOTES",
-						payload: data
-					});
-				})
-				.catch(err => {
-					console.log("session expired or smth went wrong");
-				});
-		} else {
-			console.log("not authorized");
-		}
-	};
+  return dispatch => {
+    const token = localStorage.getItem("my-jwt");
+    console.log(token);
+    if (token) {
+      return fetch(
+        "http://frontend-recruitment.one2tribe.pl:8080/api/v1/item",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+          }
+        }
+      )
+        .then(resp => resp.json())
+        .then(data => {
+          notySuccess("success");
+          dispatch({
+            type: "GET_ALL_NOTES",
+            payload: data
+          });
+        })
+        .catch(err => {
+          notyError("toke likely invalid");
+        });
+    } else {
+      console.log("not authorized");
+    }
+  };
 };
 export default fetchAllNotes;
